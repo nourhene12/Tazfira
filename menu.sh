d63f4d61759cc209c7e7e833f61aa6652c0be7b6
ghassenn <ghassen.namouchi@esprit.tn>
2019-02-25 22:31:59 +0100

    i added menu.sh

---
+++ menu.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "=================="
+echo "Menu---"
+echo "=================="
+echo "Entrer 1 pour afficher le nom du package: "
+echo "Entrer 2 pour lister les composants du package: "
+echo "Entrer 3 Afficher une description du package: "
+echo "entrer q pour quitter le menu: "
+echo -e "\n"
+echo -e "entrer votre seletion"
+read answer 
+
