ac6f11edec03e0087541b5e520d0c6a8387377c7
nourhene12 <47774281+nourhene12@users.noreply.github.com>
2019-03-01 21:32:15 +0100

    Add files via upload

--- menu.sh
+++ menu.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-echo "=================="
-echo "Menu---"
-echo "=================="
-echo "Entrer 1 pour afficher le nom du package: "
-echo "Entrer 2 pour lister les composants du package: "
-echo "Entrer 3 Afficher une description du package: "
-echo "entrer q pour quitter le menu: "
+function menu
+{
+echo "--------------- Menu----------------"
+echo "1 afficher le nom du package"
+echo "2 lister les composants du package"
+echo "3 afficher une description du package"
+echo "4 sauvgarder les noms du package 4:"
+echo "5 afficher le help 5:"
+echo "q pour quitter q:"
 echo -e "\n"
-echo -e "entrer votre seletion"
-read answer 
-
+echo -e "entrer votre selection"
+read answer
+}
+menu
